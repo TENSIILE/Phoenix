cb65e1f54ddeffaabc36751fdc6688d7a3e16ff2
TENSIILE <laptev-vlad2001@yandex.ru>
2021-09-26 20:26:33 +0300

    Memoization class created

---
+++ Core/Memo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Phoenix.Extentions;
+
+namespace Phoenix.Core
+{
+    public class Memo
+    {
+        private Store _store;
+
+        public Memo(Store store)
+        {
+            _store = store;
+        }
+
+        /// <summary>
+        /// A method observing the changes in the transmitted states.
+        /// </summary>
+        public static string[] Watch(params object[] deps)
+        {
+            return EffectDeps.Watch(deps);
+        }
+
+        /// <summary>
+        /// Memoizing data method.
+        /// </summary>
+        public void Memoize(Action callback, string[] deps)
+        {
+            foreach (string dep in deps)
+            {
+                if (_store.GetStatePrev.Has(dep) != _store.GetState.Has(dep))
+                {
+                    callback();
+                }
+            }
+        }
+    }
+}
