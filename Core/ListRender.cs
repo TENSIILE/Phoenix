be1b484ead686143e85834fde80a9628ad936670
TENSIILE <laptev-vlad2001@yandex.ru>
2022-01-28 20:05:02 +0300

    Adds a search method supposed to work in real time

--- Core/ListRender.cs
+++ Core/ListRender.cs
@@ -1,10 +1,33 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
+using Microsoft.CSharp.RuntimeBinder;
 
 namespace Phoenix.Core
 {
     public static class ListRender
     {
+        /// <summary>
+        /// Method for filtering and searching in real time.
+        /// </summary>
+        /// <param name="list">List of elements to search for.</param>
+        /// <param name="text">The text that will be checked against the list items.</param>
+        /// <param name="component">Component in which similar found elements will be displayed.</param>
+        public static void Search(List<string> list, string text, dynamic component)
+        {
+            List<string> searched = list.ToList().FindAll(v => v.ToLower().Contains(text.ToLower()));
+
+            try
+            {
+                component.Items.Clear();
+                component.Items.AddRange(searched.ToArray());
+            }
+            catch (RuntimeBinderException)
+            {
+                throw new PhoenixException("The required methods were not found for the component!", new ArgumentException());
+            }
+        }
+
         /// <summary>
         /// Method for optimized rendering of items in the list.
         /// </summary>
